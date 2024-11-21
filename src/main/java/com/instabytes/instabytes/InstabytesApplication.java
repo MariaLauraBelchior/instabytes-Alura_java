8a20ebb40b42a71d196c7f0304e46ec374355fd0
MariaLauraBelchior <marialaurabelchior007@gmail.com>
2024-11-20 23:35:48 -0300

    first commit

---
+++ src/main/java/com/instabytes/instabytes/InstabytesApplication.java
@@ -0,0 +1,13 @@
+package com.instabytes.instabytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstabytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstabytesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instabytes/instabytes/InstabytesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instabytes.instabytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstabytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
