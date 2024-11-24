4c13912c0a058bcbc42964359ecf1530d83a9bd2
MariaLauraBelchior <marialaurabelchior007@gmail.com>
2024-11-23 23:48:46 -0300

    Database conected

--- src/main/java/com/instabytes/instabytes/controllers/PostController.java
+++ src/main/java/com/instabytes/instabytes/controllers/PostController.java
@@ -2,16 +2,27 @@ package com.instabytes.instabytes.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.instabytes.instabytes.dto.PostDTO;
 import com.instabytes.instabytes.services.PostService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
-@RequestMapping(value = "/posts")
+@RequestMapping("/posts")
 public class PostController {
     
+    @Autowired
     private PostService postService;
 
 
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> findById(@PathVariable Long id) {
+        PostDTO dto = postService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
 }
--- src/main/java/com/instabytes/instabytes/services/PostService.java
+++ src/main/java/com/instabytes/instabytes/services/PostService.java
@@ -1,9 +1,11 @@
 package com.instabytes.instabytes.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import com.instabytes.instabytes.dto.PostDTO;
+import com.instabytes.instabytes.entities.Post;
 import com.instabytes.instabytes.repositories.PostRepository;
 
 @Service
@@ -12,5 +14,10 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-    
+    public PostDTO findById(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Post not found"));
+        return new PostDTO(post);
+    }
+
 }
